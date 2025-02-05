23fb810232c37c490a362aa94cc1ea0d2f38491b
kgrover3 <kgroveriii@gmail.com>
2025-02-05 15:51:01 -0500

    First calculator attempt complete

--- src/kgrover3/Main.java
+++ src/kgrover3/Main.java
@@ -1,8 +1,53 @@
 package kgrover3;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        boolean running = true;
+        boolean notunknown = true;
 
+        Scanner scanner = new Scanner(System.in);
+
+        while (running) {
+
+            System.out.println("What operation do you need? (+ - * /)");
+            String operation = scanner.next();
+
+            System.out.println("What is the first number?");
+            float numberOne = scanner.nextFloat();
+            System.out.println("What is the second number");
+            float numberTwo = scanner.nextFloat();
+            float result = 0;
+            switch (operation){
+                case "+":
+                    result = numberOne + numberTwo;
+                    break;
+                case "-":
+                    result = numberOne - numberTwo;
+                    break;
+                case "*":
+                    result = numberOne * numberTwo;
+                    break;
+                case "/":
+                    result = numberOne / numberTwo;
+                    break;
+                default:
+                    System.out.println("Unknown Operation Requested");
+                    notunknown = false;
+
+            }
+            if (notunknown) {
+
+                System.out.println(numberOne + " " + operation + " " + numberTwo + " = " + result);
+            }
+
+            System.out.println("Are you done? Y or N");
+            String done = scanner.next();
+            if (done.equals("Y") || done.equals("y")){
+                running = false;
+            }
+        }
     }
 }
